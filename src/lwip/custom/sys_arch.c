9c7f2e3788066fd643a56d2144b4f64db96d961e
eric <eric.y.corsican@gmail.com>
2024-04-27 01:15:19 +0800

    Remove the use of `mach_absolute_time` due to iOS restriction
    https://developer.apple.com/documentation/bundleresources/privacy_manifest_files/describing_use_of_required_reason_api#4278394

--- src/lwip/custom/sys_arch.c
+++ src/lwip/custom/sys_arch.c
@@ -187,25 +187,12 @@
     vprintf(format, ap);
     va_end(ap);
   }
-#elif __APPLE__
-    #include <mach/mach_time.h>
-    u32_t sys_now(void) {
-        uint64_t now = mach_absolute_time();
-        mach_timebase_info_data_t info;
-        mach_timebase_info(&info);
-        now = now * info.numer / info.denom / NSEC_PER_MSEC;
-        return (u32_t)(now);
-    }
-#elif __linux
-    #include <sys/time.h>
-    u32_t sys_now(void)
-    {
-        struct timeval te;
-        gettimeofday(&te, NULL);
-        return te.tv_sec*1000LL + te.tv_usec/1000;
-    }
-#elif __unix // all unices not caught above
-    // Unix
-#elif __posix
-    // POSIX
+#else
+  #include <sys/time.h>
+  u32_t sys_now(void)
+  {
+      struct timeval te;
+      gettimeofday(&te, NULL);
+      return te.tv_sec*1000LL + te.tv_usec/1000;
+  }
 #endif
